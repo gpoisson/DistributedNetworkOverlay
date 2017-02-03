4a29b49d84f32b77546b8500e9f9a10c71bd6bc4
Greg Poisson <gpoisson@rams.colostate.edu>
2017-02-02 21:06:01 -0700

    Messages now compute timestamps

--- src/overlay/wireformats/Message.java
+++ src/overlay/wireformats/Message.java
@@ -1,17 +1,20 @@
 package overlay.wireformats;
 
 import java.io.UnsupportedEncodingException;
-
+import java.sql.Timestamp;
+import java.util.Calendar;
 
 public abstract class Message {
 	
 	protected int type;
+	protected Timestamp time;
 	protected long timestamp;
 	protected String identifier;
 	protected int tracker;
 	protected byte[] out;
 	
 	public byte[] getByteArray() {
+		setTime();
 		String outString = type + " " + timestamp + " " + identifier + " " + tracker;
 		out = outString.getBytes();
 		return out;
@@ -26,6 +29,14 @@ public abstract class Message {
 			System.out.println(e);
 		}
 		return null;
+	}
+	
+	public void setTime() {
+
+		Calendar calendar = Calendar.getInstance();
+		java.util.Date now = calendar.getTime();
+		this.time = new java.sql.Timestamp(now.getTime());
+		timestamp = this.time.getTime();
 	}
 	
 }
