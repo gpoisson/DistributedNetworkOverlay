6f9a801b20129d8db47285b824342601d2c55abc
Greg Poisson <gpoisson@rams.colostate.edu>
2017-02-07 23:43:27 -0700

    Rewriting partition-free overlay algorithm to be effective and efficient

--- src/overlay/util/OverlayCreator.java
+++ src/overlay/util/OverlayCreator.java
@@ -24,21 +24,26 @@ public class OverlayCreator {
 	}
 
 	public void buildOverlay() {
+		/*		PARTITION-FREE OVERLAY ALGORITHM -- Greg Poisson
+		 * 			Iterate through the nodes which have the fewest number of links
+		 * 			For each node, connect it to a node with the next fewest number of links
+		 * 			Continue until all nodes have the specified number of links
+		 * 			Assume all nodes begin with zero links. Assume there are at least two nodes.
+		 */
 		if (debug) System.out.println(" Registry OverlayCreator preparing to build overlay for " + numNodes + " nodes with " + linksPerNode + " links each.");
-		if (numNodes < linksPerNode) { 
-			System.out.println(" Specified number of connections (" + linksPerNode + ") is greater than the available number of nodes (" + numNodes + "). The number of links will be adjusted."); 
-			linksPerNode = numNodes - 1;
-		}
-		for (int i = 0; i < numNodes; i++) {
-			// Iterate over each node reference, which is maintained by the registry
-			for (int link = 0; link < linksPerNode; link++) {
-				int newNeighbor = randomIntRange(0,numNodes);												// Choose a random node from the list of registered nodes 
-				while (newNeighbor == i) { newNeighbor = randomIntRange(0,numNodes); }						// If the current node was chosen, pick a different one -- don't link a node to itself
-				int newWeight = randomIntRange(1,10);														// Compute a random weight
-				LinkWeight linkWeight = new LinkWeight(nodeRefs.get(newNeighbor).getId(),newWeight);		// Define a LinkWeight object to represent the link
-				if (debug) System.out.println("\tAttempting to establish link between nodes: " + nodeRefs.get(i).getId() + ", " + nodeRefs.get(newNeighbor).getId() + " -- Link weight: " + newWeight);
-				nodeRefs.get(i).neighbors.add(linkWeight);
-				if (debug) System.out.println("\tDefined new link between nodes: " + nodeRefs.get(i).getId() + ", " + nodeRefs.get(newNeighbor).getId() + " -- Link weight: " + newWeight);
+		
+		int index_of_node_with_fewest_links = 0;
+		int fewest_number_of_links = 0;
+		int index_of_node_with_second_fewest_links = index_of_node_with_fewest_links + 1;
+		int second_fewest_number_of_links = 0;
+		int number_of_links_in_completed_overlay = numNodes * linksPerNode;
+		int current_number_of_links_in_overlay = 0;
+		
+		while (current_number_of_links_in_overlay < number_of_links_in_completed_overlay) {					// Repeat the search for incomplete nodes until all nodes are complete
+			for (int current_node_index = 0; current_node_index < numNodes; current_node_index++) {			// Iterate through nodes to find the node with the fewest links
+				if (nodeRefs.get(current_node_index).neighbors.size() < fewest_number_of_links) {
+					index_of_node_with_fewest_links = current_node_index;
+				}
 			}
 		}
 	}
